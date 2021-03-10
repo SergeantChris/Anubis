a504607a5d6cbc75c0bb5dff5fc42c25681a7b3f
George Tsagkarelis <georgets98@gmail.com>
2021-03-10 13:46:15 +0200

    Add small client update for testing

--- client/client.py
+++ client/client.py
@@ -1,5 +1,6 @@
 import requests
 import sys
+from pprint import pprint
 
 # Client is executed locally
 localhost = 'http://127.0.0.1'
@@ -7,20 +8,21 @@ localhost = 'http://127.0.0.1'
 # TODO: define way to get parameters from command line (or file?)
 
 
-def cli_insert(param):
+def cli_insert(port, param):
     call_params = {}
     requests.post(localhost + '/user/insert', call_params)
 
-def cli_delete(param):
+def cli_delete(port, param):
     call_params = {}
     requests.post(localhost + '/user/delete', call_params)
 
-def cli_query(param):
+def cli_query(port, param):
     call_params = {}
     requests.post(localhost + '/user/query', call_params)
 
 def cli_depart(port):
-    requests.post(localhost + ':' + port + '/user/depart', {})
+    response = requests.post(localhost + ':' + port + '/user/depart', {})
+    return response.text
 
 def cli_overlay(port):
     response = requests.post(localhost + ':' + port + '/user/overlay', {})
@@ -30,13 +32,17 @@ def cli_overlay(port):
 if __name__ == '__main__':
 
     # get the port from the command line
-    if len(sys.argv) < 3 or sys.argv[1] not in ('-p', '-P'):
+    if len(sys.argv) < 4 or sys.argv[1] not in ('-p', '-P') or sys.argv[3] not in ('depart', 'overlay'):
         print('Tell me the port, e.g. -p 5000')
         exit(0)
 
     port = sys.argv[2]
+    action = sys.argv[3]
 
-    # test call for depart
-    cli_depart(port)
-    # test call for overlay
-    #print(cli_overlay(port))
+    if action == 'depart':
+        print(cli_depart(port))
+
+    if action == 'overlay':
+        pprint(list(eval(cli_overlay(port))))
+    
+    exit(0)
