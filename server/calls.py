a3d3375717eaf4fd0c0e5895e396888754fe24a7
George Tsagkarelis <georgets98@gmail.com>
2021-03-10 13:46:01 +0200

    Change minor details on join/depart mechanism

--- server/calls.py
+++ server/calls.py
@@ -7,14 +7,11 @@ from config import *
 def remoteNodeUpdatePeerList(ip, port, req):
     return requests.post(HTTP + ip + ':' + port + '/node/updatePeerList', req)
 
-
 def remoteNodeQuery(ip, port, req):
     return requests.post(HTTP + ip + ':' + port + 'node/query', req)
 
-
 def remoteNodeInsert(ip, port, req):
     return requests.post(HTTP + ip + ':' + port + 'node/insert', req)
-
 
 def remoteNodeDelete(ip, port, req):
     return requests.post(HTTP + ip + ':' + port + 'node/delete', req)
--- server/utilities.py
+++ server/utilities.py
@@ -2,12 +2,15 @@ from calls import *
 from config import *
 import globals
 import hashlib
-
-
+from flask import request
 # The following functions are the handlers for API calls.
 # They use functions defined in calls.py accordingly, with respect to Chord protocol
 
-# TODO: define the logic of each handler for user/network requests
+def shutdown_server():
+    func = request.environ.get('werkzeug.server.shutdown')
+    if func is None:
+        raise RuntimeError('Not running with the Werkzeug Server')
+    func()
 
 def userInsertHandle(req):
     return 'insert'
@@ -20,9 +23,11 @@ def userQueryHandle(req):
 
 def userDepartHandle():
     depart = globals.KARNAK_ID
-    new_list = [node for node in globals.PEER_LIST if node["nid"] != depart]  # inefficient
-    requests.post(HTTP + KARNAK_MASTER_IP + ':' + KARNAK_MASTER_PORT + '/master/depart', {"new_list": str(new_list)})
-    return 'informed master'
+    # Only inform master about who you are, he will calculate the new list (issue with concurrent departs)
+    # See below on "masterDepartHandle"
+    requests.post(HTTP + KARNAK_MASTER_IP + ':' + KARNAK_MASTER_PORT + '/master/depart', {'depart_id': depart})
+    shutdown_server()
+    return 'Departing & Shutting down...'
 
 def userOverlayHandle():
     return str(globals.PEER_LIST)
@@ -32,16 +37,28 @@ def masterJoinHandle(req):
     globals.PEER_LIST.append(req)
     globals.PEER_LIST.sort(key=lambda i: (i["nid"]))
     for peer in prev_list:
-        remoteNodeUpdatePeerList(peer["ip"], peer["port"], {"new_list": str(globals.PEER_LIST)})
+        # Update anyone but master (as master)
+        if peer["nid"] != globals.KARNAK_ID:
+            remoteNodeUpdatePeerList(peer["ip"], peer["port"], {"new_list": str(globals.PEER_LIST)})
     return str(globals.PEER_LIST)
 
 def masterDepartHandle(req):
+    depart = req['depart_id']
+    new_list = [node for node in globals.PEER_LIST if node["nid"] != depart]  # inefficient, but good enough :D
+    # Update master's global peer list
+    globals.PEER_LIST = new_list
+    globals.PEER_LIST.sort(key=lambda i: (i["nid"]))
+    # Master must not call his own /node/updatePeerList -- Flask will be locked as it is able to process only 1 request
     for peer in globals.PEER_LIST:
-        remoteNodeUpdatePeerList(peer["ip"], peer["port"], req)
+        # check if I am not hitting myself (as master)
+        if peer["nid"] != globals.KARNAK_ID:
+            # give everyone the new list
+            remoteNodeUpdatePeerList(peer["ip"], peer["port"], {"new_list": str(globals.PEER_LIST)})
     return 'departed'
 
 def nodeUpdatePeerListHandle(req):
     globals.PEER_LIST = list(eval(req["new_list"]))
+    print('Updated my peer list: ', str(globals.PEER_LIST))
     return 'updated peer list'
 
 def nodeQueryHandle(req):
