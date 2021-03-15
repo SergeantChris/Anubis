8ebf9e3d98ded5da36978b48ad12b28527ec4cec
George Tsagkarelis <georgets98@gmail.com>
2021-03-15 13:35:20 +0200

    Add config entries for VM deployment

--- server/config.py
+++ server/config.py
@@ -1,8 +1,16 @@
 # Network variables
 
 KARNAK_MASTER_PORT = '5000'
-KARNAK_MASTER_IP = '127.0.0.1' #'192.168.0.1'
+
+# for local Testing
+KARNAK_MASTER_IP = '127.0.0.1'
+NETIFACE = 'lo'
+
+# for VMs
+#NETIFACE = 'ens3'
+#KARNAK_MASTER_IP = '217.69.0.179'
+
 HTTP = 'http://'
-NETIFACE = 'lo'	 # this should be eth0 or em0 in order for the vms to work
+
 global REP_K
 global CONSISTENCY_MODE
