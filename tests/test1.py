7b60ae1ed8eeca1feb4e1f8cf5599682cf0f6c79
marilenadp <marilena.depian@gmail.com>
2021-03-17 21:58:38 +0200

    added tests

--- tests/test1.py
+++ tests/test1.py
@@ -35,8 +35,12 @@ def cli_overlay(port):
 
 if __name__ == '__main__':
 
+    # list of all ports
     ports = ['5000', '5001', '5002']
+    # list of all ips
     ips = [localhost]
+
+    # test 1
     f = open("insert.txt", "r")
     inserts = f.read()
     inserts = inserts.splitlines()
@@ -44,7 +48,40 @@ if __name__ == '__main__':
         insert_list = insert.split(", ")
         song_deats = {"key": insert_list[0],
                       "value": insert_list[1]}
+        # randomly choose ip and port for insert
         port = random.choice(ports)
         cli_insert(port, song_deats)
         print(".", end ="")
-    print()
+    print("")
+
+    # test 2
+    f = open("query.txt", "r")
+    queries = f.read()
+    queries = queries.splitlines()
+    for query in queries:
+        song_deats = {"song_name": query}
+        # randomly choose ip and port for insert
+        port = random.choice(ports)
+        cli_query(port, song_deats)
+        print("-", end ="")
+    print("")
+
+    # test 3
+    f = open("requests.txt", "r")
+    requests = f.read()
+    requests = requests.splitlines()
+    for request in requests:
+        request_list = request.split(", ")
+        if request_list[0] == "insert":
+            song_deats = {"key": request_list[1],
+                          "value": request_list[2]}
+            # randomly choose ip and port for insert
+            port = random.choice(ports)
+            cli_insert(port, song_deats)
+        else:
+            song_deats = {"song_name": request_list[1]}
+            # randomly choose ip and port for insert
+            port = random.choice(ports)
+            cli_query(port, song_deats)
+        print("*", end ="")
+    print("")
