f2f66553f682df707928aa6814921282b103a16c
marilenadp <marilena.depian@gmail.com>
2021-03-19 21:28:38 +0200

    finished test script

--- tests/test1.py
+++ tests/test1.py
@@ -2,15 +2,12 @@ import requests
 import sys
 import os
 import random
-import hashlib
+import time
 
 # Client is executed locally
 localhost = 'http://127.0.0.1'
 NETIFACE = 'lo'	 # this should be eth0 or em0 in order for the vms to work
 # TODO: define way to get parameters from command line (or file?)
-
-def hash(key):
-    return hashlib.sha1(key.encode('utf-8')).hexdigest()
 
 def cli_insert(port, param):
     response = requests.post(localhost + ':' + port + '/user/insert', param)
@@ -24,15 +21,6 @@ def cli_query(port, param):
     response = requests.post(localhost + ':' + port + '/user/query', param)
     return response.text
 
-def cli_depart(port):
-    response = requests.post(localhost + ':' + port + '/user/depart', {})
-    return response.text
-
-def cli_overlay(port):
-    response = requests.post(localhost + ':' + port + '/user/overlay', {})
-    return eval(response.text)
-
-
 if __name__ == '__main__':
 
     # list of all ports
@@ -44,44 +32,49 @@ if __name__ == '__main__':
     f = open("insert.txt", "r")
     inserts = f.read()
     inserts = inserts.splitlines()
+    now = time.time()
     for insert in inserts:
         insert_list = insert.split(", ")
         song_deats = {"key": insert_list[0],
                       "value": insert_list[1]}
         # randomly choose ip and port for insert
         port = random.choice(ports)
-        cli_insert(port, song_deats)
+        print(cli_insert(port, song_deats))
         print(".", end ="")
+    later = time.time()
+    difference = later - now
     print("")
 
     # test 2
     f = open("query.txt", "r")
     queries = f.read()
     queries = queries.splitlines()
+    now = time.time()
     for query in queries:
         song_deats = {"song_name": query}
         # randomly choose ip and port for insert
         port = random.choice(ports)
         cli_query(port, song_deats)
         print("-", end ="")
+    later = time.time()
+    difference = later - now
     print("")
 
     # test 3
-    f = open("requests.txt", "r")
-    requests = f.read()
-    requests = requests.splitlines()
-    for request in requests:
-        request_list = request.split(", ")
-        if request_list[0] == "insert":
-            song_deats = {"key": request_list[1],
-                          "value": request_list[2]}
-            # randomly choose ip and port for insert
-            port = random.choice(ports)
-            cli_insert(port, song_deats)
-        else:
-            song_deats = {"song_name": request_list[1]}
-            # randomly choose ip and port for insert
-            port = random.choice(ports)
-            cli_query(port, song_deats)
-        print("*", end ="")
-    print("")
+    if len(sys.argv) == 2 and sys.argv[1] in ('-l'):
+        f = open("requests.txt", "r")
+        requests = f.read()
+        requests = requests.splitlines()
+        for request in requests:
+            request_list = request.split(", ")
+            if request_list[0] == "insert":
+                song_deats = {"key": request_list[1],
+                              "value": request_list[2]}
+                # randomly choose ip and port for insert
+                port = random.choice(ports)
+                print(cli_insert(port, song_deats))
+            else:
+                song_deats = {"song_name": request_list[1]}
+                # randomly choose ip and port for insert
+                port = random.choice(ports)
+                print(cli_query(port, song_deats))
